688fd03b427d178114eaab94540fdf2525500f11
latifersoy <132338260+latifersoy@users.noreply.github.com>
2026-06-14 19:52:27 +0300

    Update blogs.js

--- data/blogs.js
+++ data/blogs.js
@@ -13,13 +13,7 @@
 var blogPosts = [
   {
     title: "Alerjinize karşı yeni nesil Sinbiyotik Güç Probiyom Alerjen Sprey",
-    text: "Görünmez Tehdit : Toz Akrları, Polenler, evcil hayvan alerjileri. Dünya nüfusunun %35 etkileniyor. Geleneksel sorun : Kimyasallar sadece maskeleme yapar ama yok etmez ve daha kötüsü Dirençli patojen oluşumunu hızlandır Çözüm Probiyom, probiyotik teknolojisi ile doğanın kendi savunma mekanizmasını kullanır. Sinbiyotik Güç nedir ? Probiyotikler ve Prebiyorikler sinerjik etki oluşturur. Daha hızlı aktivasyon 72 saat koruma Doğal, Toksik olmayan, biyo çözünür ENZİMATİK TEMİZLİK. Bağımsız laboratuvar testleri (HeiQ Synbio verileri), sinbiyotiklerin yüzeylerdeki alerjen yükünü dramatik şekilde düşürdüğünü doğrulamaktadır (Kaynak: 2024 BMA Labor, Bochum, Germany Test Raporları): •	Ev Tozu Akarı : %96.6 •	Polen Alerjenleri : %70.8 •	Kedi Alerjenleri: %83.6 •	Köpek Alerjenleri: %78.2 
-Güvenlik ve Sürdürülebilrlik : •	Eko Uyumlu (ekolabel) •	Zararsız yapı (REACH uyumlu) •	Dengeli pH (~pH 6,5)
-Geniş Kullanım Alanı : •	Yatak ve Tekstil •	Konaklama •	Evcil Hayvan •	Sağlık
-Mikroskobik Derinlik: Geleneksel temizliğin ulaşamadığı çatlak ve gözeneklere nüfuz eder
-Uzun Süreli Etki: Haftada sadece 1-2 kez uygulama ile sürekli koruma
-Güvenli Alan: Bebekler, evcil hayvanlar ve hassas ciltler için %100 güvenli
-Koku Giderme: Organik kirliliği kaynağında yok ederek kötü kokuları önler ",
+    text: "Görünmez Tehdit : Toz Akrları, Polenler, evcil hayvan alerjileri. Dünya nüfusunun %35 etkileniyor. Geleneksel sorun : Kimyasallar sadece maskeleme yapar ama yok etmez ve daha kötüsü Dirençli patojen oluşumunu hızlandır Çözüm Probiyom, probiyotik teknolojisi ile doğanın kendi savunma mekanizmasını kullanır. Sinbiyotik Güç nedir ? Probiyotikler ve Prebiyorikler sinerjik etki oluşturur. Daha hızlı aktivasyon 72 saat koruma Doğal, Toksik olmayan, biyo çözünür ENZİMATİK TEMİZLİK. Bağımsız laboratuvar testleri (HeiQ Synbio verileri), sinbiyotiklerin yüzeylerdeki alerjen yükünü dramatik şekilde düşürdüğünü doğrulamaktadır (Kaynak: 2024 BMA Labor, Bochum, Germany Test Raporları): •	Ev Tozu Akarı : %96.6 •	Polen Alerjenleri : %70.8 •	Kedi Alerjenleri: %83.6 •	Köpek Alerjenleri: %78.2 Güvenlik ve Sürdürülebilrlik : •	Eko Uyumlu (ekolabel) •	Zararsız yapı (REACH uyumlu) •	Dengeli pH (~pH 6,5)Geniş Kullanım Alanı : •	Yatak ve Tekstil •	Konaklama •	Evcil Hayvan •	SağlıkMikroskobik Derinlik: Geleneksel temizliğin ulaşamadığı çatlak ve gözeneklere nüfuz eder. Uzun Süreli Etki: Haftada sadece 1-2 kez uygulama ile sürekli koruma Güvenli Alan: Bebekler, evcil hayvanlar ve hassas ciltler için %100 güvenli. Koku Giderme: Organik kirliliği kaynağında yok ederek kötü kokuları önler",
     imageDir: "images/blogs/blog_1_alerji.jpg",
     imageName: "Alerjen Sprey ile rahat nefes, ferah ortamlar "
   }
