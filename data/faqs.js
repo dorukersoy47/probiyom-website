2b3ef830f772ca66652a27bd7d66b061b850c620
latifersoy <132338260+latifersoy@users.noreply.github.com>
2026-06-17 01:52:54 +0300

    Update faqs.js

--- data/faqs.js
+++ data/faqs.js
@@ -9,5 +9,9 @@ var faqs = [
   {
     question: "Probiyotik temizlik ürünleri nedir ve nasıl çalışır?",
     answer: "Probiyotik temizlik ürünlerimiz, faydalı bakterilerin (probiyotikler) ve onların besinlerinin (prebiyotikler) patentli birleşimini içerir. Yüzeylere uygulandıktan sonra sürekli enzim üreterek organik kirleri parçalar ve yüzeylerde dengeli bir mikrobiyom oluşmasına yardımcı olur. Bu sayede temizlik etkisi kuruduktan sonra da uzun süre devam eder."
-  }
+  },
+  {
+    question: "Heiq VivoTech Zemin ve İç Mekan ürünleri arasında ki fark nedir?",
+    answer: "İç mekan hertürlü masa tezgah kapı vs gibi sert yüzeylerde kullanılırken Zemin adı üsütünde suya dayanıklı hertürlü zemin ve yer temizliğinde kullanılır. Her ikisi de toza karşı uzun süreli etki yaratı."
+  },
 ];
