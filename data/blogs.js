2f3008dc5ae84354e3167003daf384cb06020a44
latifersoy <132338260+latifersoy@users.noreply.github.com>
2026-06-17 01:00:56 +0300

    Update blogs.js

--- data/blogs.js
+++ data/blogs.js
@@ -4,7 +4,7 @@
 
    Each entry: {
      title:    "...",
-     text:     "...",        // use \n\n for paragraph breaks
+     text:     "...",        // use \n\n for paragraph breaks 
      imageDir: "images/...", // recommended size: 800x700 (8:7)
      imageName:"alt text"
    }
@@ -12,7 +12,7 @@
 
 var blogPosts = [
   {
-    title: "Alerjinize karşı yeni nesil Sinbiyotik Güç",
+    title: "/strong Alerjinize karşı yeni nesil Sinbiyotik Güç",
     text: "Görünmez Tehdit : Toz Akarları, Polenler, evcil hayvan alerjileri. Dünya nüfusunun %35 etkileniyor.\n Geleneksel sorun : Kimyasallar sadece maskeleme yapar ama yok etmez ve daha kötüsü Dirençli patojen oluşumunu hızlandır. \n Çözüm : Probiyom, probiyotik teknolojisi ile doğanın kendi savunma mekanizmasını kullanır.\n Sinbiyotik Güç nedir ? Probiyotikler ve Prebiyorikler sinerjik etki oluşturur. Daha hızlı aktivasyon 72 saat koruma.\n Doğal, Toksik olmayan, biyo çözünür ENZİMATİK TEMİZLİK. Bağımsız laboratuvar testleri (HeiQ Synbio verileri), sinbiyotiklerin yüzeylerdeki alerjen yükünü dramatik şekilde düşürdüğünü doğrulamaktadır (Kaynak: 2024 BMA Labor, Bochum, Germany Test Raporları): \n•	Ev Tozu Akarı : %96.6 \n•	Polen Alerjenleri : %70.8 \n•	Kedi Alerjenleri: %83.6 \n•	Köpek Alerjenleri: %78.2 \n Güvenlik ve Sürdürülebilrlik : \n•	Eko Uyumlu (ekolabel) \n•	Zararsız yapı (REACH uyumlu) \n•	Dengeli pH (~pH 6,5) \nGeniş Kullanım Alanı : \n•	Yatak ve Tekstil \n•	Konaklama \n•	Evcil Hayvan \n•	Sağlık \nMikroskobik Derinlik: Geleneksel temizliğin ulaşamadığı çatlak ve gözeneklere nüfuz eder. \n Uzun Süreli Etki: Haftada sadece 1-2 kez uygulama ile sürekli koruma \n Güvenli Alan: Bebekler, evcil hayvanlar ve hassas ciltler için %100 güvenli. \n Koku Giderme: Organik kirliliği kaynağında yok ederek kötü kokuları önler",
     imageDir: "images/blogs/blog_1_alerji.jpg",
     imageName: "Alerjen Sprey ile rahat nefes, ferah ortamlar "
